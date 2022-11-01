ed8ab2caf741bf503d12c757c87f232a201e9cb6
makki9532 <makki9532@gmail.com>
2022-11-01 18:39:48 +0000

    Update settings.py

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7ppocbnx@w71dcuinn*t^_mzal(t@o01v3fee27g%rg18fc5d@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
